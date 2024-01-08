588d4cd4c7040de8cbe5ad22ec84bfa9777b103a
marziyenajafzadeh <marziyenajafzade@gmail.com>
2024-01-08 16:31:10 +0330

    Update Crypto API to use API key and async/await

--- src/assets/Components/Templates/HomePage.jsx
+++ src/assets/Components/Templates/HomePage.jsx
@@ -5,8 +5,12 @@ import { getCoinList } from "../../../sevices/CryptoApi";
 function HomePage() {
     const [coins, setCoins] = useState([]);
     useEffect(() => {
-        fetch(getCoinList())
-            .then(res => res.json()).then((json) => setCoins(json))
+        const getData = async () => {
+            const res = await fetch(getCoinList())
+            const json = await res.json();
+            setCoins(json)
+        }
+        getData();
     }, [])
     return (
         <div>
--- src/sevices/CryptoApi.js
+++ src/sevices/CryptoApi.js
@@ -1,5 +1,8 @@
+const BASE_URL = "https://api.coingecko.com/api/v3";
+const API_KEY ="CG-XZuFbXpGvN4W8UAanGjJ3TYv";
+
 const getCoinList = () =>{
-    return "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&x_cg_demo_api_key=CG-XZuFbXpGvN4W8UAanGjJ3TYv"
+    return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&x_cg_demo_api_key=${API_KEY}`
 
 };
 export { getCoinList }
